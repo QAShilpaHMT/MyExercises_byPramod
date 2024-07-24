8410fed35fc2914a3fc6e549953dfa2850400d63
dhruvalbarmeda2210 <Dhruval.barmeda@mvp1ventures.com>
2024-07-24 06:32:49 +0530

    all

---
+++ src/Exe07072024/Lab001.java
@@ -0,0 +1,41 @@
+package Exe07072024;
+
+public class Lab001 {
+
+    public static void main(String[] args) {
+
+        //print sentence
+        System.out.println("Hello ,Good Morning");
+
+        //Print values stored in variables
+
+        int a =100;
+        double b =1.11;
+        double c,d,e,f;
+        String str = "Shilpa here";
+        double dbl = 11.11;
+        boolean bln = true;
+        System.out.println("Value of a is "+ a);
+       // System.out.println("Value of b is "+ b);
+        System.out.println("Value of str is "+ str);
+        System.out.println("Value of dbl is "+ dbl);
+        System.out.println("Value of a boolean "+ bln);
+
+
+
+
+        //Print values of simple calcumations like addition,subtraction,multiplication,division
+
+        c= a+ b;
+        d = b/a;
+        e =a*b;
+        f = b-a ;
+
+        System.out.println("Value of a a +b is  "+ c);
+        System.out.println("Value of a b/a  is  "+ d);
+        System.out.println("Value of a a*b is  "+ e);
+        System.out.println("Value of a b-a is  "+ f);
+
+
+    }
+}
